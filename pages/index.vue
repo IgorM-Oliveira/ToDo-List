b72215bab13f69916d25b724b035afb76b4c0c90
IgorM-Oliveira <igor.matos.oliveira.xyz@gmail.com>
2021-03-03 11:38:43 -0400

    Preparando o css do to-do list

--- pages/index.vue
+++ pages/index.vue
@@ -1,27 +1,19 @@
 <template>
-  <div class="container">
-    <div>
-      <Logo />
-      <h1 class="title">
-        To-Do_List
-      </h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
+  <div>
+    <div class="container">
+      <div class="card">
+        <div class="border">
+          <h1 class="title">
+            Lista para Tudo
+          </h1>
+          <form class="add">
+            <label>
+              <input type="text" placeholder="Adicionar um item">
+            </label>
+            <button>+</button>
+          </form>
+          <Logo />
+        </div>
       </div>
     </div>
   </div>
@@ -32,47 +24,47 @@
 </script>
 
 <style>
-/* Sample `apply` at-rules with Tailwind CSS
-.container {
-@apply min-h-screen flex justify-center items-center text-center mx-auto;
+button {
+  border: 2px solid #2ECC71;
+  border-radius: 100%;
+  padding-right: 10px;
+  padding-left: 10px;
 }
-*/
+
+input {
+  border: 2px solid #2ECC71;
+  border-radius: 10px;
+  padding-left: 15px;
+}
+
+.add {
+  padding-top: 10px;
+  padding-bottom: 15px;
+}
+
 .container {
   margin: 0 auto;
   min-height: 100vh;
   display: flex;
   justify-content: center;
   align-items: center;
-  text-align: center;
 }
 
 .title {
-  font-family:
-    'Quicksand',
-    'Source Sans Pro',
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
   display: block;
   font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
+  font-size: 25px;
   letter-spacing: 1px;
 }
 
-/*.subtitle {*/
-/*  font-weight: 300;*/
-/*  font-size: 42px;*/
-/*  color: #526488;*/
-/*  word-spacing: 5px;*/
-/*  padding-bottom: 15px;*/
-/*}*/
+.card {
+  padding-left: 15%;
+  padding-right: 15%;
+}
 
-.links {
-  padding-top: 15px;
+.border {
+  border: 5px solid #2ECC71;
+  padding: 1rem;
+  border-radius: 10px;
 }
 </style>
